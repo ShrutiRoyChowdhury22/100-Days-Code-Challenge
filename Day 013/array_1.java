ff0ea8302b7be948a6caed09ab8c6a3052957f66
ShrutiRoyChowdhury22 <114934032+ShrutiRoyChowdhury22@users.noreply.github.com>
2022-11-13 21:42:26 +0530

    Add files via upload

---
+++ Day 013/array_1.java
@@ -0,0 +1,13 @@
+public class array_1 {
+    public static void main(String[] args) {
+        int m[] = new int[5];
+        m[0] = 90;
+        m[1] = 78;
+        m[2] = 80;
+        m[3] = 95;
+        m[4] = 89;
+        for (int i = 0; i < 5; i++){
+            System.out.println(m[i]);
+        }
+    }
+}
