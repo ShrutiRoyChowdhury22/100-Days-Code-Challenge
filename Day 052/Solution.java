eaf7449f75f4cc4834e8409cd3f638e4dbca278a
ShrutiRoyChowdhury22 <114934032+ShrutiRoyChowdhury22@users.noreply.github.com>
2023-01-26 15:43:03 +0530

    Create Solution.java

---
+++ Day 052/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null){
+            return false;
+        }
+        ListNode firstPtr = head;
+        ListNode SecondPtr = head.next;
+        while(firstPtr!= SecondPtr){
+             
+             if(SecondPtr==null || SecondPtr.next==null){
+                 return false;
+             }
+             firstPtr=firstPtr.next;
+             SecondPtr=SecondPtr.next.next;
+
+        }
+        return true;
+    }
+}
